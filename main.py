a9c8109e71d86757d5e16192d8103ba2773d02ca
Kostr0min <60927437+Kostr0min@users.noreply.github.com>
2021-08-07 06:38:01 +0300

    Dev (#2)
    * add test
    
    * Develop/lint test ci (#1)
    
    * add actions
    
    * remove empty action

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
+import pandas as pd
 
+from tools.data_scanner import DataScanner
+
+test_df = pd.read_csv('tests/test_data.csv')
+data_scanner = DataScanner('tests/test_json.json')
+# data_scanner.scan(test_df, 'test', 'v_2')
+# data_scanner.dump_to_json()
+test_df = test_df.append(test_df.astype(str))
+val = data_scanner.check_for_types_in_every_column(test_df)
+print(val)
--- tools/data_scanner.py
+++ tools/data_scanner.py
@@ -1,7 +1,9 @@
-import pandas as pd
-from typing import List
-import numpy as np
 import json
+from json import JSONDecodeError
+from typing import List
+
+import numpy as np
+import pandas as pd
 
 
 class DataScanner:
@@ -9,6 +11,7 @@ class DataScanner:
     .scan() dataframes
     .dump_to_json(path) dump all scanned dataframe configs to path
     """
+
     def __init__(self, path_to_conf_json: str = None):
         self.path_to_conf_json = path_to_conf_json
         self.collected_configs = self.init_config_dict
@@ -23,11 +26,13 @@ class DataScanner:
         conf_dict = {}
         if self.path_to_conf_json:
             try:
-                with open(self.path_to_conf_json, 'r') as f:
+                with open(self.path_to_conf_json) as f:
                     conf_dict = json.load(f)
-            except FileNotFoundError:
-                print(f'Ignore this warning if new json. \n'
-                      f'Invalid path to config json: {self.path_to_conf_json}')
+            except (FileNotFoundError, JSONDecodeError):
+                print(
+                    f'Ignore this warning if new json. \n'
+                    f'Invalid path to config json: {self.path_to_conf_json}',
+                )
         return conf_dict
 
     def dump_to_json(self, path: str = None):
@@ -39,12 +44,35 @@ class DataScanner:
             print(f'Invalid path to configuraton json: {path_to_config}')
 
     @staticmethod
+    def check_for_types_in_every_column(dataframe: pd.DataFrame):
+        """
+        Check all columns values by type() and store unique types with column key
+
+        govnocode edition
+
+        :param dataframe:
+        :return:
+        """
+        types_dict = {}
+        for col in dataframe.columns:
+            val = dataframe[col].apply(lambda x: type(x)).unique()
+            types_dict[col] = [str(x)[8:-2] for x in val]
+            if len(val) > 1:
+                print(
+                    f'WARNING, NOT ALL VALUES ARE OF THE SAME TYPE. COLUMN {col}\n',
+                    f'TYPES {val}',
+                )
+        return types_dict
+
+    @staticmethod
     def statistics_extractor(dataframe: pd.DataFrame, numeric_columns: List[str] = None):
         """
         Simple pandas describe data extractor
         numeric_columns: list of numeric columns names
         """
-        columns_list = numeric_columns if numeric_columns else dataframe.select_dtypes(include=np.number).columns
+        columns_list = numeric_columns if numeric_columns else dataframe.select_dtypes(
+            include=np.number,
+        ).columns
         return dataframe[columns_list].describe().to_dict()
 
     def scan(self, dataframe: pd.DataFrame, name: str = None, version: str = None, blank_shot: bool = False):
@@ -68,13 +96,15 @@ class DataScanner:
         >>> data_scanner.scan(df, 'digits_and_words', 'version_1')
 
         """
-        config_dict = {'dtypes_': dataframe.dtypes.apply(lambda x: x.name).to_dict(),
-                       'columns_': dataframe.columns.to_list(), 'shape_': list(dataframe.shape),
-                       'numeric_columns_stats': self.statistics_extractor(dataframe)}
+        config_dict = {
+            'dtypes_': dataframe.dtypes.apply(lambda x: x.name).to_dict(),
+            'columns_': dataframe.columns.to_list(), 'shape_': list(dataframe.shape),
+            'numeric_columns_stats': self.statistics_extractor(dataframe),
+            'unique_types': self.check_for_types_in_every_column(dataframe),
+        }
         if blank_shot:
             return config_dict
         if name not in self.collected_configs.keys():
             self.collected_configs[name] = {}
         self.collected_configs[name][version] = config_dict
         self.collected_configs[name]['last_version'] = version
-
