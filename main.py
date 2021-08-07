9badfeaf2e398a755fcc74d4a6e842ecce5e5007
Kostr0min <60927437+Kostr0min@users.noreply.github.com>
2021-08-07 06:36:07 +0300

    Develop/lint test ci (#1)
    * add actions
    
    * remove empty action

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-from tools.data_scanner import DataScanner
 import pandas as pd
 
-test_df = pd.read_csv("tests/test_data.csv")
-data_scanner = DataScanner("tests/test_json.json")
-#data_scanner.scan(test_df, 'test', 'v_2')
-#data_scanner.dump_to_json()
+from tools.data_scanner import DataScanner
+
+test_df = pd.read_csv('tests/test_data.csv')
+data_scanner = DataScanner('tests/test_json.json')
+# data_scanner.scan(test_df, 'test', 'v_2')
+# data_scanner.dump_to_json()
 test_df = test_df.append(test_df.astype(str))
 val = data_scanner.check_for_types_in_every_column(test_df)
 print(val)
